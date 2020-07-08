37f2456fa624ddcfd57e253a2c3c9122ba586f1a
SeanPower <seanmp22@gmail.com>
2020-07-08 11:57:19 -0400

    Refactored to do away with intermediate variables and got navbar links attached

--- public/main.js
+++ public/main.js
@@ -4,40 +4,38 @@ let myMap = (mapboxgl.accessToken =
 let map = new mapboxgl.Map({
   container: "map", // container id
   style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
-  center: [-73.2150, 44.4810], // starting position [lng, lat]
+  center: [-73.215, 44.481], // starting position [lng, lat]
   zoom: 15, // starting zoom
 });
-let restoNameArray = [];
-let restoCoords = [];
-let restaurantList = document.getElementById('restoList')
 
 // Function Declarations ////////////////////////////////////
-function placeMarker(coords) {  // Place marker on map
-       let lngLatArr = [coords[1], coords[0]];
-       new mapboxgl.Marker().setLngLat(lngLatArr).addTo(map);
-    };
-
-
-async function restaurantsOnMap() {  // Fetch API data and push relavent data to arrays for later use
-    let restaurantList = await fetch('https://yelpingtonapi.herokuapp.com/api/restaurants')
-    .then((res) => res.json())
-    
-    
-    for (restaurant of restaurantList) {
-        restoNameArray.push(restaurant.name)
-        restoCoords.push(restaurant.coords)
-    }
-    restoCoords.forEach(coords => {
-        placeMarker(coords) // Loop to place Lng/Lat markers on map
-    })
-    restoNameArray.forEach(restaurant => { // Loop to fill Nav Bar with restaurant name data
-      let node = document.createElement('LI')
-      let textNode = document.createTextNode(restaurant)
-      node.appendChild(textNode)
-      document.getElementById('restoList').appendChild(node);
-      })
+function placeMarker(coords) {
+  // Place marker on map
+  let lngLatArr = [coords[1], coords[0]];
+  new mapboxgl.Marker().setLngLat(lngLatArr).addTo(map);
 }
 
+async function restaurantsOnMap() {
+  // Fetch API data and push relavent data to arrays for later use
+  let restaurantList = await fetch(
+    "https://yelpingtonapi.herokuapp.com/api/restaurants"
+  ).then((res) => res.json());
+  console.log(restaurantList);
+
+  for (restaurant of restaurantList) {
+    let node = document.createElement("LI");
+    let textNode = document.createTextNode(restaurant.name);
+    let a = document.createElement("a");
+    
+    placeMarker(restaurant.coords);
+
+    a.appendChild(textNode);
+    a.title = restaurant.name;
+    a.href = "/restaurant#" + restaurant.id;
+    node.appendChild(a);
+    document.getElementById("restoList").appendChild(node);
+  }
+}
 
 // Function Call ////////////////////////////////////////////
 restaurantsOnMap();
